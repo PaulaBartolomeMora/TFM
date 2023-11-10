6d81ec0832753a6b3715246b66df7e7fb3c97c84
Paula B <72092576+PaulaBartolomeMora@users.noreply.github.com>
2023-11-10 17:33:54 +0100

    new env_prod.csv, mean function

--- SustData/datasamples.py
+++ SustData/datasamples.py
@@ -29,24 +29,48 @@ X_features.append('iid')
 ################################################################################################################
 #función de cálculo de la media del total de valores medidos en una hora determinada para un nodo determinado
 ################################################################################################################
-def extract(IID_SAMPLE, HOUR_SAMPLE, DATETIME_SAMPLE):
+def extract_mean(IID_SAMPLE, HOUR_SAMPLE, DATETIME_SAMPLE):
     filter = df[(df['iid'] == IID_SAMPLE) & (df['H'] == HOUR_SAMPLE) & (df['datetime'] == DATETIME_SAMPLE)] #filtro parámetros input
     filter.head()
 
-    hourly_data = pd.DataFrame() #df de almacenamiento
+    df = pd.DataFrame() #df de almacenamiento
  
     for feature in tqdm(X_features, desc=f'Procesando IID {IID_SAMPLE}'):
         if feature == 'iid':
-            hourly_data[feature] = IID_SAMPLE
+            df[feature] = IID_SAMPLE
         else:
-            hourly_data[feature] = [filter[feature].mean()] #se almacena la media de todas las features según los parámetros input dados 
+            df[feature] = [filter[feature].mean()] #se almacena la media de todas las features según los parámetros input dados 
 
-    return hourly_data    
+    return df  
+  
+
+################################################################################################################
+#función de cálculo de la media del total de valores de Pavg en un período de tiempo 
+################################################################################################################
+def Pavg_mean(IID_SAMPLE, DATETIME_SAMPLE):
+    filter = df[(df['iid'] == IID_SAMPLE) & (df['datetime'] == DATETIME_SAMPLE)] #filtro parámetros input
+    filter.head()
+
+    df = pd.DataFrame() #df de almacenamiento
+ 
+    for feature in tqdm(X_features, desc=f'Procesando IID {IID_SAMPLE}'):
+        if feature == 'iid':
+            df[feature] = IID_SAMPLE
+        else:
+            df[feature] = [filter[feature].mean()] #se almacena la media de todas las features según los parámetros input dados 
+
+    return df  
+
 
 
 iid_array = df['iid'].unique() #ids nodos que aparecen en el dataset
 print(iid_array) 
 
-extraction = [extract(iid, HOUR_SAMPLE, DATETIME_SAMPLE) for iid in iid_array] #titulos + filas
+""" extraction = [extract_mean(iid, HOUR_SAMPLE, DATETIME_SAMPLE) for iid in iid_array] #titulos + filas
 new_df = pd.concat(extraction, ignore_index=True) #único titulo + filas
-new_df.to_csv('OUT.csv', index=False)
+new_df.to_csv('OUT.csv', index=False) """
+
+
+
+media = Pavg_mean(42, DATETIME_SAMPLE)
+media.head()
