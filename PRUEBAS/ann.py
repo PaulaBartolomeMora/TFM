c7c42d6149f1d7fd7e81b26cc3bf4dcf1b017642
Paula <pbm4@hotmail.es>
2024-02-29 11:05:07 +0100

    pruebas ann, escrito rf en estado del arte

---
+++ PRUEBAS/ann.py
@@ -0,0 +1,149 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras 
+from scikeras.wrappers import KerasClassifier
+import glob as gl
+import os
+
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.neural_network import MLPClassifier
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+import time
+
+##############################################################
+
+path = "src/results"
+dfs = []
+
+for file in gl.glob(path + '/20*.csv'):
+    dfs.append(pd.read_csv(file))
+
+df = pd.concat(dfs, ignore_index=True)
+
+modelo = df.iloc[:, 8].values 
+modelo = LabelEncoder().fit_transform(modelo) #codificación del modelo
+df = df.drop(df.columns[8], axis=1) #se elimina la antigua con los strings del modelo
+df['modelo'] = modelo #se añade la nueva codificada al final
+
+X = df.iloc[:, 1:] 
+X = X.drop(['datetime', 'timestamp', 'load', 'DC Array Output (W)' , 'Pavg', 'dif'], axis=1)
+y = df.iloc[:, 0].values #valores de overflow
+
+##############################################################
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+sc = StandardScaler() #escalado de datos
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+##############################################################
+
+def ann():
+    model = keras.Sequential([
+        
+        #Adding the input layer and the first hidden layer
+        #se prueba con 6, los valores se obtienen probando o con alguna técnica como k Fold Cross Validation
+        keras.layers.Dense(7, input_shape=(X.shape[1],), activation='relu'), #nº de entradas
+        
+        #Adding the second hidden layer
+        #mismo valor que arriba
+        keras.layers.Dense(7, activation='tanh'),
+        
+        #Adding the output layer -> 1 salida: 0 o 1
+        #*si no es salida binaria, se indica el número de posibles salidas y activation='softmax'
+        keras.layers.Dense(1, activation='sigmoid')
+    ]) 
+
+    model.summary() #resumen de la estructura de la red neuronal
+    #los parámetros son los pesos
+
+    inicl = time.time()
+    model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    History = model.fit(X_train, y_train, batch_size = 50, epochs = 20)
+    fincl = time.time()
+
+##############################################################
+
+    plt.figure(figsize=(10, 4))
+    plt.plot(History.history['loss'])
+    plt.title('Evolución de la función de pérdidas')
+    plt.ylabel('Pérdidas')
+    plt.xlabel('Epochs')
+    plt.savefig('loss.png', bbox_inches='tight')
+    plt.show()
+
+    plt.figure(figsize=(10, 4))
+    plt.plot(History.history['accuracy'])
+    plt.title('Evolución de la precisión')
+    plt.ylabel('Precisión')
+    plt.xlabel('Epochs')
+    plt.savefig('accuracy.png', bbox_inches='tight')
+    plt.show()
+
+##############################################################
+
+    test_loss, test_accuracy = model.evaluate(X_test, y_test)
+
+    y_pred = model.predict(X_test)
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+    accuracy_score(y_test, y_pred)
+
+    return model,inicl,fincl
+
+
+##############################################################
+##############################################################
+##############################################################
+
+m,inicl,fincl = ann()
+model = KerasClassifier(build_fn=ann)
+
+parameters = {
+    'hidden_layer_sizes': [(5,5,1), (6,6,1), (7,7,1), (8,8,1)],
+    'batch_size': [20, 50, 80],
+    'epochs': [10, 30, 50],
+    'activation': ['sigmoid', 'tanh', 'relu'],
+}
+
+processors = 32
+cv = 5 
+combos = 1
+
+for i in parameters:
+    for j in i.values():
+        combos *= len(j)
+
+num_models = combos * cv / processors 
+seconds = num_models * (fincl-inicl)
+minutes = seconds / 60
+hours = minutes / 60
+
+print("{:.6f}".format(hours), "| {:.6f}".format(minutes), "| {:.6f}".format(seconds))
+
+##############################################################
+
+inigs = time.time()
+
+grid_search = GridSearchCV(estimator = model,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 5,
+                           n_jobs = -1)
+
+grid_search.fit(X_train, y_train)
+
+fings = time.time()
+
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
+print("Best Accuracy: {:.2f} %".format(best_accuracy*100))
+print("Best Parameters:", best_parameters)
+print("Tiempo ejecución grid search: " + str(fings-inigs))
---
+++ PRUEBAS/svm.py
@@ -0,0 +1,207 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import glob as gl
+import os
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.svm import SVC 
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import RFE, SelectKBest, chi2
+from sklearn.inspection import permutation_importance
+
+import time
+
+##############################################################
+
+path = "src/results"
+dfs = []
+
+for file in gl.glob(path + '/20*.csv'):
+    dfs.append(pd.read_csv(file))
+
+df = pd.concat(dfs, ignore_index=True)
+
+modelo = df.iloc[:, 8].values 
+modelo = LabelEncoder().fit_transform(modelo) #codificación del modelo
+df = df.drop(df.columns[8], axis=1) #se elimina la antigua con los strings del modelo
+df['modelo'] = modelo #se añade la nueva codificada al final
+
+X = df.iloc[:, 1:] 
+X = X.drop(['datetime', 'timestamp', 'load', 'DC Array Output (W)' , 'Pavg', 'dif'], axis=1)
+y = df.iloc[:, 0].values #valores de overflow
+
+##############################################################
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+sc = StandardScaler() #escalado de datos
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+##############################################################
+
+#APLICACIÓN PCA
+
+pca = PCA(n_components=3)
+X_train_pca = pca.fit_transform(X_train)
+X_test_pca = pca.transform(X_test)
+
+##############################################################
+
+""" #APLICACIÓN FEATURE SELECTION, ESTO YA ESTÁ EJECUTADO
+
+scaler = MinMaxScaler() # Escalar las características al rango [0, 1]
+
+select_feature = SelectKBest(chi2, k=8).fit(scaler.fit_transform(X_train), y_train) 
+selected_features = X.columns[select_feature.get_support(indices=True)]
+print('Selected features:', selected_features)
+
+X_train_fs = select_feature.transform(X_train)
+X_test_fs = select_feature.transform(X_test)"""
+
+##############################################################
+
+print("Ejecutando Clasificador")
+
+inicl = time.time()
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
+fincl = time.time()
+
+print("Tiempo ejecución clasificador 1: " + str(fincl-inicl)) 
+
+##############################################################
+
+""" ESTO YA ESTÁ EJECUTADO
+# Importancia de las características basada en la distancia a los vectores de soporte
+
+support_vectors = classifier.support_vectors_ #vectores de soporte
+dual_coef = classifier.dual_coef_ #multiplicadores de Lagrange asociados
+print("Vectores soporte y coeficientes: ")
+print(support_vectors)
+print(dual_coef)
+
+importances = np.abs(np.dot(dual_coef, support_vectors)).flatten()
+importances_series = pd.Series(importances, index=X.columns)
+plt.figure(figsize=(10, 6))
+importances_series.plot(kind='bar')
+plt.ylabel('Importancia')
+plt.title('Importancia de las características del SVC con kernel RBF')
+plt.xticks(rotation=45, ha='right')
+plt.show() """
+
+##############################################################
+
+""" 
+# Feature importance based on mean decrease in impurity --> #ESTO DA ERROR, SVC NO DEJA OBTENERLO ASÍ
+features_ranking = pd.Series(classifier.feature_importances_, index=X.columns)
+
+plt.figure(figsize=(10, 6))  
+features_ranking.plot(kind='bar')
+plt.ylabel('Importancia')
+plt.title('Importancia de las características del RF a partir de la Disminución Media de la Impureza (MDI)')
+plt.xticks(rotation=45, ha='right')
+plt.show() """
+
+##############################################################
+
+# Feature importance based on feature permutation
+
+result = permutation_importance(
+    classifier, X_test, y_test, n_repeats=5, random_state=42, n_jobs=2
+)
+
+importances_mean_series = pd.Series(result.importances_mean, index=X.columns)
+
+plt.figure(figsize=(10, 6))
+importances_mean_series.plot(kind='bar')
+plt.ylabel('Importancia')
+plt.title('Importancia de las características del RF a partir de la permutación')
+plt.xticks(rotation=45, ha='right')
+plt.savefig('importance4.png', bbox_inches='tight')
+plt.show()
+
+##############################################################
+
+#APLICACIÓN RFE (RFECV NO SE PUEDE CON SVC, NO TIENE ATRIBUTO feature_importances_)
+
+print("Ejecutando RFE")
+
+rfe = RFE(estimator=classifier, n_features_to_select=5, step=1)  
+rfe = rfe.fit(X_train, y_train)
+
+print('Ranking de features :', rfe.ranking_)
+print('Mejores features :', X.columns[rfe.support_])
+
+
+##############################################################
+
+##K-FOLD
+
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100)) 
+
+classifier.fit(X_train_pca, y_train) 
+accuracies = cross_val_score(estimator = classifier, X = X_train_pca, y = y_train, cv = 10)
+
+print("Accuracy PCA: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation PCA: {:.2f} %".format(accuracies.std()*100)) 
+
+##############################################################
+
+##GRID SEARCH
+
+""" parameters = [{'C': [0.25, 0.5, 0.75, 1], 'kernel': ['linear']},
+              {'C': [0.25, 0.5, 0.75, 1], 'kernel': ['rbf']}]
+
+processors = 32
+cv = 5 
+combos = 1
+
+for i in parameters:
+    for j in i.values():
+        combos *= len(j)
+
+num_models = combos * cv / processors 
+seconds = num_models * (fincl-inicl)
+minutes = seconds / 60
+hours = minutes / 60
+
+print("{:.6f}".format(hours), "| {:.6f}".format(minutes), "| {:.6f}".format(seconds))
+
+
+inigs = time.time()
+
+print("Ejecutando Grid Search")
+
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 5,
+                           n_jobs = -1)
+
+grid_search.fit(X_train, y_train)
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
+print("Best Accuracy: {:.2f} %".format(best_accuracy*100))
+print("Best Parameters:", best_parameters)
+print(grid_search.best_estimator_) 
+
+fings = time.time()
+
+print("Tiempo ejecución grid search: " + str(fings-inigs))
+
+print("Imprimiendo reporte")
+
+y_pred_gs = grid_search.predict(X_test) 
+print(classification_report(y_test, y_pred_gs))  """
